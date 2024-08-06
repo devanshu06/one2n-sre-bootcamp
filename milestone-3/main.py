d751abe3e093cfd370f9ed07bf9f3330d0f43943
Devanshu Singh <devuvns@gmail.com>
2024-08-06 14:16:37 +0530

    added the logic for Database schema migration using flaskmigrate

--- milestone-3/main.py
+++ milestone-3/main.py
@@ -1,10 +1,14 @@
 import os
 import logging
 from flask import Flask, request
-import mysql.connector
-from mysql.connector import pooling, Error
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from dotenv import load_dotenv
+import mysql.connector
+from mysql.connector import Error
 import db_queries
+
+app = Flask(__name__)
 
 def setup_environment_and_logging():
     load_dotenv()
@@ -29,11 +33,7 @@ def setup_environment_and_logging():
 
     return DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME, FLASK_HOST, FLASK_PORT, DEBUG_MODE
 
-DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME, FLASK_HOST, FLASK_PORT, DEBUG_MODE = setup_environment_and_logging()
-
-app = Flask(__name__)
-
-def initialize_database():
+def create_database_if_not_exists():
     try:
         connection = mysql.connector.connect(
             host=DATABASE_HOST,
@@ -42,8 +42,6 @@ def initialize_database():
         )
         cursor = connection.cursor()
         cursor.execute(f"CREATE DATABASE IF NOT EXISTS {DATABASE_NAME} DEFAULT CHARACTER SET 'utf8'")
-        connection.database = DATABASE_NAME
-        cursor.execute(db_queries.CREATE_STUDENT_TABLE)
         connection.commit()
     except Error as e:
         logging.error(f"Error initializing database: {str(e)}")
@@ -51,132 +49,94 @@ def initialize_database():
         cursor.close()
         connection.close()
 
-    dbconfig = {
-        "host": DATABASE_HOST,
-        "user": DATABASE_USER,
-        "password": DATABASE_PASSWORD,
-        "database": DATABASE_NAME,
-    }
+DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME, FLASK_HOST, FLASK_PORT, DEBUG_MODE = setup_environment_and_logging()
 
-    return pooling.MySQLConnectionPool(pool_name="mypool",
-                                       pool_size=10,
-                                       pool_reset_session=True,
-                                       **dbconfig)
+create_database_if_not_exists()
 
-connection_pool = initialize_database()
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-def get_db_connection():
-    try:
-        connection = connection_pool.get_connection()
-        return connection
-    except Error as e:
-        logging.error(f"Error connecting to database: {str(e)}")
-        return None
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+class Student(db.Model):
+    __tablename__ = 'students'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    # age = db.Column(db.Integer, nullable=False)
 
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
     return {'status': 'OK'}, 200
 
-
 @app.post('/api/v1/addstudent')
 def addstudent():
-    connection = get_db_connection()
-    if not connection:
-        return {"error": "Database connection failed"}, 500
     data = request.get_json()
     if "name" not in data:
         return {"error": "name field is required"}, 400
     name = data["name"]
     try:
-        cursor = connection.cursor()
-        cursor.execute(db_queries.INSERT_STUDENT, (name,))
-        connection.commit()
-        cursor.execute("SELECT LAST_INSERT_ID()")
-        student_id = cursor.fetchone()[0]
-        return {"id": student_id, "message": f"Student {name} created."}, 201
+        new_student = Student(name=name)
+        db.session.add(new_student)
+        db.session.commit()
+        return {"id": new_student.id, "message": f"Student {name} created."}, 201
     except Exception as e:
         logging.error(f"Error occurred: {str(e)}")
         return {"error": str(e)}, 500
 
-
 @app.delete('/api/v1/deletestudent/<int:num>')
 def deletestudent(num):
-    connection = get_db_connection()
-    if not connection:
-        return {"error": "Database connection failed"}, 500
     try:
-        cursor = connection.cursor()
-        cursor.execute(db_queries.STUDENT_BY_ID, (num,))
-        student = cursor.fetchone()
+        student = Student.query.get(num)
         if not student:
             return {"message": f"No record found for student with id {num}."}, 404
-        cursor.execute(db_queries.DELETE_STUDENT, (num,))
-        connection.commit()
+        db.session.delete(student)
+        db.session.commit()
         return {"message": f"Student with id {num} deleted."}, 200
     except Exception as e:
         logging.error(f"Error occurred: {str(e)}")
         return {"error": str(e)}, 500
 
-
 @app.get('/api/v1/allstudent')
 def allstudent():
-    connection = get_db_connection()
-    if not connection:
-        return {"error": "Database connection failed"}, 500
     try:
-        cursor = connection.cursor()
-        cursor.execute(db_queries.ALL_STUDENT)
-        students = cursor.fetchall()
-        student_list = [{"id": student[0], "name": student[1]} for student in students]
+        students = Student.query.all()
+        student_list = [{"id": student.id, "name": student.name} for student in students]
         return {"students": student_list}, 200
     except Exception as e:
         logging.error(f"Error occurred: {str(e)}")
         return {"error": str(e)}, 500
 
-
 @app.get('/api/v1/student/<int:num>')
 def studentbyid(num):
-    connection = get_db_connection()
-    if not connection:
-        return {"error": "Database connection failed"}, 500
     try:
-        cursor = connection.cursor()
-        cursor.execute(db_queries.STUDENT_BY_ID, (num,))
-        student = cursor.fetchone()
+        student = Student.query.get(num)
         if student is None:
             return {"message": f"Student with id {num} not found."}, 404
-        return {"id": num, "name": student[0]}, 200
+        return {"id": num, "name": student.name}, 200
     except Exception as e:
         logging.error(f"Error occurred: {str(e)}")
         return {"error": str(e)}, 500
 
-
 @app.put('/api/v1/updatestudent/<int:num>')
 def updatestudentbyid(num):
-    connection = get_db_connection()
-    if not connection:
-        return {"error": "Database connection failed"}, 500
     data = request.get_json()
     if "name" not in data:
         return {"error": "Name field is required"}, 400
     new_name = data["name"]
     try:
-        cursor = connection.cursor()
-        cursor.execute(db_queries.STUDENT_BY_ID, (num,))
-        student = cursor.fetchone()
+        student = Student.query.get(num)
         if student is None:
             return {"error": f"Student with id {num} not found."}, 404
-        current_name = student[0]
+        current_name = student.name
         if current_name == new_name:
             return {"message": "No Change in name detected. Nothing needs to be updated."}, 200
-        cursor.execute(db_queries.UPDATE_STUDENT_BY_ID, (new_name, num))
-        connection.commit()
+        student.name = new_name
+        db.session.commit()
         return {"message": f"Student with id:{num} updated from {current_name} to {new_name}."}, 200
     except Exception as e:
         logging.error(f"Error occurred: {str(e)}")
         return {"error": str(e)}, 500
-
 
 if __name__ == '__main__':
     app.run(host=FLASK_HOST, port=FLASK_PORT, debug=DEBUG_MODE)
