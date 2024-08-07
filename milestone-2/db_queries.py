bd301d9c584343e430474d1da96b9d3ec2805e7a
Devanshu Singh <devuvns@gmail.com>
2024-08-07 14:56:17 +0530

    changed the db queries from tuple to string

--- milestone-2/db_queries.py
+++ milestone-2/db_queries.py
@@ -1,18 +1,6 @@
-CREATE_STUDENT_TABLE = (
-    "CREATE TABLE IF NOT EXISTS student (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL);"
-)
-INSERT_STUDENT = (
-    "INSERT INTO student (name) VALUES (%s);"
-)
-DELETE_STUDENT = (
-    "DELETE FROM student WHERE id = %s;"
-)
-ALL_STUDENT = (
-    "SELECT * from student;"
-)
-STUDENT_BY_ID = (
-    "SELECT name from student where id = %s;"
-)
-UPDATE_STUDENT_BY_ID = (
-    "UPDATE student set name = %s where id = %s"
-)
+CREATE_STUDENT_TABLE = "CREATE TABLE IF NOT EXISTS student (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL);"
+INSERT_STUDENT = "INSERT INTO student (name) VALUES (%s);"
+DELETE_STUDENT = "DELETE FROM student WHERE id = %s;"
+ALL_STUDENT = "SELECT * from student;"
+STUDENT_BY_ID = "SELECT name from student where id = %s;"
+UPDATE_STUDENT_BY_ID = "UPDATE student set name = %s where id = %s;"
